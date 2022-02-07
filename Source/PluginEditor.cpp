8d3618c587bf78d124ac99f7d4c9788101c96d2f
cjedward <cjedward@umich.edu>
2022-02-07 14:23:42 -0500

    Began implementing knobs of the VST

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -8,14 +8,37 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+using namespace juce;
 
 //==============================================================================
-Distortion_VSTAudioProcessorEditor::Distortion_VSTAudioProcessorEditor (Distortion_VSTAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+Distortion_VSTAudioProcessorEditor::Distortion_VSTAudioProcessorEditor(Distortion_VSTAudioProcessor& p)
+    : AudioProcessorEditor(&p), audioProcessor(p)
 {
+    /////////////////////// Creates knob with text, sets style and size ///////
+    // Drive knob
+    addAndMakeVisible(driveKnob = new Slider("Drive"));
+    driveKnob->setSliderStyle(Slider::Rotary);
+    driveKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+
+    // Range knob
+    addAndMakeVisible(rangeKnob = new Slider("Range"));
+    driveKnob->setSliderStyle(Slider::Rotary);
+    driveKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+
+    // Blend knob
+    addAndMakeVisible(blendKnob = new Slider("Blend"));
+    driveKnob->setSliderStyle(Slider::Rotary);
+    driveKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+
+    // Gain knob
+    addAndMakeVisible(gainKnob = new Slider("Gain"));
+    driveKnob->setSliderStyle(Slider::Rotary);
+    driveKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize(500, 200);
 }
 
 Distortion_VSTAudioProcessorEditor::~Distortion_VSTAudioProcessorEditor()
@@ -23,14 +46,14 @@ Distortion_VSTAudioProcessorEditor::~Distortion_VSTAudioProcessorEditor()
 }
 
 //==============================================================================
-void Distortion_VSTAudioProcessorEditor::paint (juce::Graphics& g)
+void Distortion_VSTAudioProcessorEditor::paint(juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour(juce::Colours::white);
+    g.setFont(15.0f);
+    g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void Distortion_VSTAudioProcessorEditor::resized()
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,24 +10,41 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "../JuceLibraryCode/JuceHeader.h"
+
 
 //==============================================================================
 /**
 */
-class Distortion_VSTAudioProcessorEditor  : public juce::AudioProcessorEditor
+class Distortion_VSTAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    Distortion_VSTAudioProcessorEditor (Distortion_VSTAudioProcessor&);
+    Distortion_VSTAudioProcessorEditor(Distortion_VSTAudioProcessor&);
     ~Distortion_VSTAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
+
+    juce::ScopedPointer<juce::Slider> driveKnob;
+    juce::ScopedPointer<juce::Slider> rangeKnob;
+    juce::ScopedPointer<juce::Slider> blendKnob;
+    juce::ScopedPointer<juce::Slider> gainKnob;
+
+    ///////// allows you to share parameters betwween class, enabling presets ??????
+    juce::ScopedPointer<juce::AudioProcessorValueTreeState::SliderAttachment> driveAttachment;
+    juce::ScopedPointer<juce::AudioProcessorValueTreeState::SliderAttachment> rangeAttachment;
+    juce::ScopedPointer<juce::AudioProcessorValueTreeState::SliderAttachment> blendAttachment;
+    juce::ScopedPointer<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
+
+
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Distortion_VSTAudioProcessor& audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Distortion_VSTAudioProcessorEditor)
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Distortion_VSTAudioProcessorEditor)
 };
